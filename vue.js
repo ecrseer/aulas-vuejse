12ab51f0861bfeeec4bca9dfc0bb86bf640eb34f
ecrseer <juustino@live.com>
2021-07-22 00:33:20 -0300

    editar e remover

--- vue.js
+++ vue.js
@@ -4,14 +4,22 @@ const app = new Vue({
     el:'#app',
     data:{
         tarefas:[
-            'limpar a casa'
+            'limpar a casa',
+            'lavar a louca'
         ],
         campoTexto:''
     },
     methods:{
         adicionar(){
             this.tarefas.push(this.campoTexto)
+        },
+        remover(indice){
+            this.tarefas.splice(indice,1)
+        },
+        editar(indice){
+            this.tarefas[indice] = this.campoTexto
         }
+
     }
 
 })
